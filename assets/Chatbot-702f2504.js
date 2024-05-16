d55c8a18b4d1a4da48be3880ceeaa7751cfd58c0
donnik007 <56132989+donnik007@users.noreply.github.com>
2024-05-17 00:37:46 +0200

    Add files via upload

---
+++ assets/Chatbot-702f2504.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c,a as o}from"./index-72d2ae5b.js";const s={},a=o("h1",null,"chat",-1),n=[a];function _(r,l){return t(),c("div",null,n)}const f=e(s,[["render",_]]);export{f as default};
